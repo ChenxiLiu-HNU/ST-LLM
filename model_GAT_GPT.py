1cb2fce4c2981c5cfa6745239778b1d9760c7d63
Chenxi Liu <46647878+ChenxiLiu-HNU@users.noreply.github.com>
2024-09-03 09:52:49 +0800

    Update model_GAT_GPT.py

--- model_GAT_GPT.py
+++ model_GAT_GPT.py
@@ -76,17 +76,14 @@ class ST_LLM(nn.Module):
         return sum([param.nelement() for param in self.parameters()])
 
     def forward(self, history_data):
-        input_data = history_data
-        batch_size, _, num_nodes, _ = input_data.shape
-
-        history_data = history_data.permute(0, 3, 2, 1)
+        batch_size, _, num_nodes, _ = history_data.shape
 
         # reshape
-        input_data = input_data.transpose(1, 2).contiguous()
-        input_data = (
-            input_data.view(batch_size, num_nodes, -1).transpose(1, 2).unsqueeze(-1)
+        history_data = history_data.transpose(1, 2).contiguous()
+        history_data = (
+            history_data.view(batch_size, num_nodes, -1).transpose(1, 2).unsqueeze(-1)
         )
-        data_st = self.start_conv(input_data)
+        data_st = self.start_conv(history_data)
 
         data_st = data_st.permute(0, 2, 3, 1)
         data_st = data_st.view(batch_size, num_nodes, -1)
