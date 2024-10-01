0fe2ce4d5209a6e06d77426f25cf03a13d0fc50b
Chenxi Liu <46647878+ChenxiLiu-HNU@users.noreply.github.com>
2024-10-01 11:19:43 +0800

    Add files via upload

--- train.py
+++ train.py
@@ -1,5 +1,4 @@
 import torch
-import torch.optim as optim
 import numpy as np
 import pandas as pd
 import argparse
@@ -8,65 +7,67 @@ import util
 import os
 from util import *
 import random
-from model_ST_LLM import ST_LLM
+from model.ST_LLM_LoRA_ufzFFN import ST_LLM
 from ranger21 import Ranger
+os.environ['PYTORCH_CUDA_ALLOC_CONF'] = 'max_split_size_mb:21'
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--device", type=str, default="cuda:0", help="")
+parser.add_argument("--device", type=str, default="cuda:6", help="")
 parser.add_argument("--data", type=str, default="bike_drop", help="data path")
 parser.add_argument("--input_dim", type=int, default=3, help="input_dim")
-parser.add_argument("--channels", type=int, default=64, help="number of features")
 parser.add_argument("--num_nodes", type=int, default=250, help="number of nodes")
 parser.add_argument("--input_len", type=int, default=12, help="input_len")
 parser.add_argument("--output_len", type=int, default=12, help="out_len")
 parser.add_argument("--batch_size", type=int, default=64, help="batch size")
-parser.add_argument("--learning_rate", type=float, default=0.001, help="learning rate")
-parser.add_argument("--dropout", type=float, default=0.1, help="dropout rate")
-parser.add_argument(
-    "--weight_decay", type=float, default=0.0001, help="weight decay rate"
-)
-parser.add_argument("--epochs", type=int, default=500, help="200~500")
+parser.add_argument("--lrate", type=float, default=1e-3, help="learning rate")
+parser.add_argument("--llm_layer", type=int, default=1, help="llm layer")
+parser.add_argument("--U", type=int, default=2, help="unforzen layer")
+parser.add_argument("--epochs", type=int, default=300, help="500")
 parser.add_argument("--print_every", type=int, default=50, help="")
+parser.add_argument(
+    "--wdecay", type=float, default=0.0001, help="weight decay rate"
+    )
 parser.add_argument(
     "--save",
     type=str,
     default="./logs/" + str(time.strftime("%Y-%m-%d-%H:%M:%S")) + "-",
-    help="save path",
-)
+    help="save path"
+    )
 parser.add_argument(
     "--es_patience",
     type=int,
     default=100,
-    help="quit if no improvement after this many iterations",
-)
+    help="quit if no improvement after this many iterations"
+    )
 args = parser.parse_args()
 
 class trainer:
     def __init__(
         self,
         scaler,
+        lrate,
+        wdecay,
         input_dim,
-        channels,
         num_nodes,
         input_len,
         output_len,
-        dropout,
-        lrate,
-        wdecay,
+        llm_layer, 
+        U,
         device
     ):
         self.model = ST_LLM(
-            device, input_dim, channels, num_nodes, input_len, output_len, dropout
+            input_dim, num_nodes, input_len, output_len, llm_layer, U, device
         )
         self.model.to(device)
+        
         self.optimizer = Ranger(self.model.parameters(), lr=lrate, weight_decay=wdecay)
         # self.optimizer = optim.Adam(self.model.parameters(), lr=lrate, weight_decay=wdecay)
+        
         self.loss = util.MAE_torch
         self.scaler = scaler
         self.clip = 5
         print("The number of parameters: {}".format(self.model.param_num()))
         print(self.model)
-        # exit()
 
     def train(self, input, real_val):
         self.model.train()
@@ -118,17 +119,16 @@ def main():
     elif args.data == "bike_pick":
         args.data = "data//" + args.data
         args.num_nodes = 250
-    
+
     elif args.data == "taxi_drop":
         args.data = "data//" + args.data
         args.num_nodes = 266
 
     elif args.data == "taxi_pick":
         args.data = "data//" + args.data
-        args.num_nodes = 266
+        args.num_nodes = 266    
     
     device = torch.device(args.device)
-
     dataloader = util.load_dataset(
         args.data, args.batch_size, args.batch_size, args.batch_size
     )
@@ -144,7 +144,6 @@ def main():
     train_time = []
     result = []
     test_result = []
-
     print(args)
 
     if not os.path.exists(path):
@@ -152,21 +151,19 @@ def main():
 
     engine = trainer(
         scaler,
+        args.lrate,
+        args.wdecay,
         args.input_dim,
-        args.channels,
         args.num_nodes,
         args.input_len,
         args.output_len,
-        args.dropout,
-        args.learning_rate,
-        args.weight_decay,
-        device,
-    )
+        args.llm_layer,
+        args.U,
+        device
+        )
 
     print("start training...", flush=True)
-
     for i in range(1, args.epochs + 1):
-        # train
         train_loss = []
         train_mape = []
         train_rmse = []
@@ -175,7 +172,7 @@ def main():
         t1 = time.time()
         # dataloader['train_loader'].shuffle()
         for iter, (x, y) in enumerate(dataloader["train_loader"].get_iterator()):
-            trainx = torch.Tensor(x).to(device)
+            trainx = torch.Tensor(x).to(device)  # 64 12 250 1
             trainx = trainx.transpose(1, 3)
             trainy = torch.Tensor(y).to(device)
             trainy = trainy.transpose(1, 3)
@@ -185,18 +182,18 @@ def main():
             train_rmse.append(metrics[2])
             train_wmape.append(metrics[3])
 
-            if iter % args.print_every == 0:
-                log = "Iter: {:03d}, Train Loss: {:.4f}, Train RMSE: {:.4f}, Train MAPE: {:.4f}, Train WMAPE: {:.4f}"
-                print(
-                    log.format(
-                        iter,
-                        train_loss[-1],
-                        train_rmse[-1],
-                        train_mape[-1],
-                        train_wmape[-1],
-                    ),
-                    flush=True,
-                )
+            # if iter % args.print_every == 0:
+                # log = "Iter: {:03d}, Train Loss: {:.4f}, Train RMSE: {:.4f}, Train MAPE: {:.4f}, Train WMAPE: {:.4f}"
+                # print(
+                #     log.format(
+                #         iter,
+                #         train_loss[-1],
+                #         train_rmse[-1],
+                #         train_mape[-1],
+                #         train_wmape[-1],
+                #     ),
+                #     flush=True,
+                # )
         t2 = time.time()
         log = "Epoch: {:03d}, Training Time: {:.4f} secs"
         print(log.format(i, (t2 - t1)))
@@ -237,6 +234,8 @@ def main():
         mvalid_rmse = np.mean(valid_rmse)
 
         his_loss.append(mvalid_loss)
+        print("-----------------------")
+
         train_m = dict(
             train_loss=np.mean(train_loss),
             train_rmse=np.mean(train_rmse),
@@ -263,16 +262,23 @@ def main():
 
         if mvalid_loss < loss:
             print("###Update tasks appear###")
-            if i < 100:
+            if i <= 100:
                 # It is not necessary to print the results of the test set when epoch is less than 100, because the model has not yet converged.
                 loss = mvalid_loss
                 torch.save(engine.model.state_dict(), path + "best_model.pth")
                 bestid = i
                 epochs_since_best_mae = 0
-                print("Updating! Valid Loss:", mvalid_loss, end=", ")
+                print("Updating! Valid Loss:{:.4f}".format(mvalid_loss), end=", ")
+                print("epoch: ", i)
+                
+            else: 
+                loss = mvalid_loss
+                torch.save(engine.model.state_dict(), path + "best_model.pth")
+                bestid = i
+                epochs_since_best_mae = 0
+                print("Updating! Valid Loss:{:.4f}".format(mvalid_loss), end=", ")
                 print("epoch: ", i)
 
-            elif i > 100:
                 outputs = []
                 realy = torch.Tensor(dataloader["y_test"]).to(device)
                 realy = realy.transpose(1, 3)[:, 0, :, :]
@@ -291,26 +297,17 @@ def main():
                 amape = []
                 awmape = []
                 armse = []
-                test_m = []
 
                 for j in range(args.output_len):
                     pred = scaler.inverse_transform(yhat[:, :, j])
                     real = realy[:, :, j]
                     metrics = util.metric(pred, real)
-                    log = "Evaluate best model on test data for horizon {:d}, Test MAE: {:.4f}, Test RMSE: {:.4f}, Test MAPE: {:.4f}, Test WMAPE: {:.4f}"
-                    print(
-                        log.format(
-                            j + 1, metrics[0], metrics[2], metrics[1], metrics[3]
-                        )
-                    )
-
-                    test_m = dict(
-                        test_loss=np.mean(metrics[0]),
-                        test_rmse=np.mean(metrics[2]),
-                        test_mape=np.mean(metrics[1]),
-                        test_wmape=np.mean(metrics[3]),
-                    )
-                    test_m = pd.Series(test_m)
+                    # log = "Evaluate best model on test data for horizon {:d}, Test MAE: {:.4f}, Test RMSE: {:.4f}, Test MAPE: {:.4f}, Test WMAPE: {:.4f}"
+                    # print(
+                    #     log.format(
+                    #         j + 1, metrics[0], metrics[2], metrics[1], metrics[3]
+                    #     )
+                    # )
 
                     amae.append(metrics[0])
                     amape.append(metrics[1])
@@ -326,13 +323,7 @@ def main():
 
                 if np.mean(amae) < test_log:
                     test_log = np.mean(amae)
-                    loss = mvalid_loss
-                    torch.save(engine.model.state_dict(), path + "best_model.pth")
-                    epochs_since_best_mae = 0
-                    print("Test low! Updating! Test Loss:", np.mean(amae), end=", ")
-                    print("Test low! Updating! Valid Loss:", mvalid_loss, end=", ")
-                    bestid = i
-                    print("epoch: ", i)
+                    print(f"Test low! Updating! Test Loss: {test_log:.4f}, Valid Loss: {mvalid_loss:.4f}, epoch: {i}")
                 else:
                     epochs_since_best_mae += 1
                     print("No update")
@@ -343,6 +334,8 @@ def main():
 
         train_csv = pd.DataFrame(result)
         train_csv.round(8).to_csv(f"{path}/train.csv")
+
+        # Early stop
         if epochs_since_best_mae >= args.es_patience and i >= 200:
             break
 
@@ -381,8 +374,8 @@ def main():
         pred = scaler.inverse_transform(yhat[:, :, i])
         real = realy[:, :, i]
         metrics = util.metric(pred, real)
-        log = "Evaluate best model on test data for horizon {:d}, Test MAE: {:.4f}, Test RMSE: {:.4f}, Test MAPE: {:.4f}, Test WMAPE: {:.4f}"
-        print(log.format(i + 1, metrics[0], metrics[2], metrics[1], metrics[3]))
+        # log = "Evaluate best model on test data for horizon {:d}, Test MAE: {:.4f}, Test RMSE: {:.4f}, Test MAPE: {:.4f}, Test WMAPE: {:.4f}"
+        # print(log.format(i + 1, metrics[0], metrics[2], metrics[1], metrics[3]))
 
         test_m = dict(
             test_loss=np.mean(metrics[0]),
@@ -414,6 +407,7 @@ def main():
     test_csv.round(8).to_csv(f"{path}/test.csv")
 
 if __name__ == "__main__":
+    torch.cuda.empty_cache()
     t1 = time.time()
     main()
     t2 = time.time()
